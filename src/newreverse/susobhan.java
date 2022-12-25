811777151c86d6b1bb4925073c68bcd986365777
susobhan lal <you@example.com>
2022-12-25 08:30:13 +0530

    first commit

---
+++ src/newreverse/susobhan.java
@@ -0,0 +1,29 @@
+package newreverse;
+
+public class susobhan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+
+		
+		String s= "susobhan  hi main ";
+		int n = s.length();
+		String sum ="";
+		for(int i=n-1;i>=0;i--) {
+			char word= s.charAt(i);
+			sum+=word;
+		}
+		System.out.print(sum+" ");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
